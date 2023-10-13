78fbaca61cecd5784dc326c00983515fbe6d98c4
xWyvernPx <phonglethanh2@gmail.com>
2023-10-14 02:11:26 +0700

    HK-52 (...) update sidebar

--- src/base/menu-items/@homekitchen/admin.tsx
+++ src/base/menu-items/@homekitchen/admin.tsx
@@ -52,134 +52,141 @@ export const admin: NavItemType = {
     {
       id: "order",
       title: <FormattedMessage id="order" />,
-      type: "collapse",
+      type: "item",
       icon: icons.SnippetsOutlined,
-      children: [
-        {
-          id: "order-list",
-          title: <FormattedMessage id="list" />,
-          type: "item",
-          icon: icons.TableOutlined,
-          url: "/order/list",
-        },
-        {
-          id: "order-analytics",
-          title: <FormattedMessage id="analytics" />,
-          type: "item",
-          url: "/order/analytic",
-          icon: icons.AreaChartOutlined,
-        },
-      ],
+      url: "/order/list",
+
+      // children: [
+      //   {
+      //     id: "order-list",
+      //     title: <FormattedMessage id="list" />,
+      //     type: "item",
+      //     icon: icons.TableOutlined,
+      //     url: "/order/list",
+      //   },
+      //   {
+      //     id: "order-analytics",
+      //     title: <FormattedMessage id="analytics" />,
+      //     type: "item",
+      //     url: "/order/analytic",
+      //     icon: icons.AreaChartOutlined,
+      //   },
+      // ],
     },
     {
       id: "customer",
       title: <FormattedMessage id="customer" />,
-      type: "collapse",
+      type: "item",
       icon: icons.CustomerServiceOutlined,
-      children: [
-        {
-          id: "customer-list",
-          title: <FormattedMessage id="list" />,
-          type: "item",
-          icon: icons.TableOutlined,
-          url: "/customer/list",
-        },
-        {
-          id: "customer-analytics",
-          title: <FormattedMessage id="analytics" />,
-          type: "item",
-          url: "/customer/analytic",
-          icon: icons.AreaChartOutlined,
-        },
-      ],
+      url: "/customer/list"
+      // children: [
+      //   {
+      //     id: "customer-list",
+      //     title: <FormattedMessage id="list" />,
+      //     type: "item",
+      //     icon: icons.TableOutlined,
+      //     url: "/customer/list",
+      //   },
+      //   {
+      //     id: "customer-analytics",
+      //     title: <FormattedMessage id="analytics" />,
+      //     type: "item",
+      //     url: "/customer/analytic",
+      //     icon: icons.AreaChartOutlined,
+      //   },
+      // ],
     },
     {
       id: "kitchen",
       title: <FormattedMessage id="kitchen" />,
-      type: "collapse",
+      type: "item",
       icon: icons.ForkOutlined,
-      children: [
-        {
-          id: "kitchen-list",
-          title: <FormattedMessage id="list" />,
-          type: "item",
-          icon: icons.TableOutlined,
-          url: "/kitchen/list",
-        },
-        {
-          id: "kitchen-analytics",
-          title: <FormattedMessage id="analytics" />,
-          type: "item",
-          url: "/kitchen/analytic",
-          icon: icons.AreaChartOutlined,
-        },
-      ],
+      url: "/kitchen/list"
+      // children: [
+      //   {
+      //     id: "kitchen-list",
+      //     title: <FormattedMessage id="list" />,
+      //     type: "item",
+      //     icon: icons.TableOutlined,
+      //     url: "/kitchen/list",
+      //   },
+      //   {
+      //     id: "kitchen-analytics",
+      //     title: <FormattedMessage id="analytics" />,
+      //     type: "item",
+      //     url: "/kitchen/analytic",
+      //     icon: icons.AreaChartOutlined,
+      //   },
+      // ],
     },
     {
       id: "area",
       title: <FormattedMessage id="area" />,
-      type: "collapse",
+      type: "item",
       icon: icons.ShopOutlined,
-      children: [
-        {
-          id: "area-list",
-          title: <FormattedMessage id="list" />,
-          type: "item",
-          icon: icons.TableOutlined,
-          url: "/area/list",
-        },
-        {
-          id: "area-analytics",
-          title: <FormattedMessage id="analytics" />,
-          type: "item",
-          url: "/area/analytic",
-          icon: icons.AreaChartOutlined,
-        },
-      ],
+      url: "/area/list"
+      // children: [
+      //   {
+      //     id: "area-list",
+      //     title: <FormattedMessage id="list" />,
+      //     type: "item",
+      //     icon: icons.TableOutlined,
+      //     url: "/area/list",
+      //   },
+      //   {
+      //     id: "area-analytics",
+      //     title: <FormattedMessage id="analytics" />,
+      //     type: "item",
+      //     url: "/area/analytic",
+      //     icon: icons.AreaChartOutlined,
+      //   },
+      // ],
     },
     {
       id: "feedback",
       title: <FormattedMessage id="feedback" />,
-      type: "collapse",
+      type: "item",
       icon: icons.ShopOutlined,
-      children: [
-        {
-          id: "feedback-list",
-          title: <FormattedMessage id="list" />,
-          type: "item",
-          icon: icons.TableOutlined,
-          url: "/feedback/list",
-        },
-        {
-          id: "feedback-analytics",
-          title: <FormattedMessage id="analytics" />,
-          type: "item",
-          url: "/feedback/analytic",
-          icon: icons.AreaChartOutlined,
-        },
-      ],
+
+      // children: [
+      //   {
+      //     id: "feedback-list",
+      //     title: <FormattedMessage id="list" />,
+      //     type: "item",
+      //     icon: icons.TableOutlined,
+      //     url: "/feedback/list",
+      //   },
+      //   {
+      //     id: "feedback-analytics",
+      //     title: <FormattedMessage id="analytics" />,
+      //     type: "item",
+      //     url: "/feedback/analytic",
+      //     icon: icons.AreaChartOutlined,
+      //   },
+      // ],
     },
     {
       id: "payment",
       title: <FormattedMessage id="payment" />,
       type: "collapse",
       icon: icons.ShopOutlined,
-      children: [
-        {
-          id: "payment-list",
-          title: <FormattedMessage id="list" />,
-          type: "item",
-          icon: icons.TableOutlined,
-          url: "/payment/list",
-        },
-        {
-          id: "payment-analytics",
-          title: <FormattedMessage id="analytics" />,
-          type: "item",
-          url: "/payment-type/analytic",
-          icon: icons.AreaChartOutlined,
-        },
-      ],
+      url: "/payment/list",
+      // children: [
+      //   {
+      //     id: "payment-list",
+      //     title: <FormattedMessage id="list" />,
+      //     type: "item",
+      //     icon: icons.TableOutlined,
+      //     url: "/payment/list",
+      //   },
+      //   {
+      //     id: "payment-analytics",
+      //     title: <FormattedMessage id="analytics" />,
+      //     type: "item",
+      //     url: "/payment-type/analytic",
+      //     icon: icons.AreaChartOutlined,
+      //   },
+      // ],
     },
   ],
 };
